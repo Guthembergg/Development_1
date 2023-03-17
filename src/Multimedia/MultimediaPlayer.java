ddc030224deb3a7b8f343e721206829ff80b526f
Karim Guettech <karimguettech@gmail.com>
2023-03-17 15:49:30 +0100

    added player

--- src/Multimedia/MultimediaPlayer.java
+++ src/Multimedia/MultimediaPlayer.java
@@ -3,47 +3,66 @@ package Multimedia;
 import java.util.Scanner;
 
 public class MultimediaPlayer {
+    static Scanner input = new Scanner(System.in);
+    static ElementoMultimediale[] media = new ElementoMultimediale[5];
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        ElementoMultimediale[] media = new ElementoMultimediale[5];
+        creaPlayer();
+        playNow();
+        input.close();
+    }
+
+    public static void creaPlayer() {
 
         // Creazione degli elementi multimediale letti da tastiera
         for (int i = 0; i < media.length; i++) {
             System.out.println("Inserisci il tipo di elemento multimediale (1 per Immagine, 2 per Video, 3 per Registrazione Audio):");
+
             int tipo = input.nextInt();
             input.nextLine();
 
-            System.out.println("Inserisci il titolo:");
-            String titolo = input.nextLine();
 
+            switch (tipo) {
+                case 1:
+                    System.out.println("Inserisci il titolo:");
+                    String titolo = input.nextLine();
+                    System.out.println("Inserisci la luminosità:");
+                    int luminosita = input.nextInt();
+                    media[i] = new Immagine(titolo, luminosita);
+                    break;
+                case 2:
+                    System.out.println("Inserisci il titolo:");
+                    String titolo2 = input.nextLine();
+                    System.out.println("Inserisci la durata:");
+                    int durata2 = input.nextInt();
+                    System.out.println("Inserisci il volume:");
+                    int volume2 = input.nextInt();
+                    System.out.println("Inserisci la luminosità:");
+                    int luminosita2 = input.nextInt();
+                    media[i] = new Video(titolo2, durata2, volume2, luminosita2);
+                    break;
+                case 3:
+                    System.out.println("Inserisci il titolo:");
+                    String titolo3 = input.nextLine();
+                    System.out.println("Inserisci la durata:");
+                    int durata3 = input.nextInt();
+                    System.out.println("Inserisci il volume:");
+                    int volume3 = input.nextInt();
+                    media[i] = new Audio(titolo3, durata3, volume3);
+                    break;
+                default:
+                    i--;
+                    System.out.println("Scelta inesistente riprova");
+                    break;
 
-            if (tipo == 1) {
-                System.out.println("Inserisci la luminosità:");
-                int luminosita = input.nextInt();
-
-                media[i] = new Immagine(titolo, luminosita);
-            } else if (tipo == 2) {
-                System.out.println("Inserisci la durata:");
-                int durata = input.nextInt();
-
-                System.out.println("Inserisci il volume:");
-                int volume = input.nextInt();
-
-                System.out.println("Inserisci la luminosità:");
-                int luminosita = input.nextInt();
-
-                media[i] = new Video(titolo, durata, volume, luminosita);
-            } else if (tipo == 3) {
-                System.out.println("Inserisci la durata:");
-                int durata = input.nextInt();
-
-                System.out.println("Inserisci il volume:");
-                int volume = input.nextInt();
-
-                media[i] = new Audio(titolo, durata, volume);
             }
+
         }
 
+
+    }
+
+    public static void playNow() {
         // Esecuzione degli elementi multimediale scelti dall'utente
         int scelta = -1;
         while (scelta != 0) {
@@ -51,16 +70,18 @@ public class MultimediaPlayer {
             scelta = input.nextInt();
 
             if (scelta >= 1 && scelta <= 5) {
-                ElementoMultimediale elem = media[scelta-1];
+                ElementoMultimediale elem = media[scelta - 1];
 
                 if (elem instanceof Immagine) {
                     ((Immagine) elem).show();
                 } else if (elem instanceof Riproducibile) {
                     ((Riproducibile) elem).play();
                 }
+            } else {
+                System.out.println("Scelta inesistente, riprova");
             }
         }
-
-        input.close();
     }
+
+
 }
